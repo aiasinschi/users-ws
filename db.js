56d63a1156a02e80ccfee891271fcd55c3d654a5
Adrian Iasinschi <adrian.iasinschi@kepler-rominfo.com>
2017-11-09 15:40:03 +0200

    Added some debug information for the connection.

--- db.js
+++ db.js
@@ -5,4 +5,7 @@ const mongoPass = process.env.MONGO_PASS || '';
 const mongoDBName = process.env.MONGO_DBNAME || 'users';
 const mongoDBUrl = process.env.MONGO_DBURL || 'localhost:27017';
 const url = 'mongodb://' + mongoUser + ':' + mongoPass + '@' + mongoDBUrl + '/' + mongoDBName;
-mongoose.connect(url);
+mongoose.connect(url, {},  function(error) {
+    console.log(error);
+    console.log(mongoUser + ' / ' + mongoPass + ' / ' + mongoDBName + ' / ' + mongoDBUrl);
+});
